9d6d410189dd7216a4932f0f77402491d4dec632
Téo <teo.villet2@gmail.com>
2023-10-14 17:28:38 +0200

    -Implémentation des routes 'api/users' dans le server

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-import express from 'express';
-import 'dotenv/config';
-import mongoose from 'mongoose';
+import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import UsersRoutes from './routes/usersRoutes.js'
 
 //express app
-const app = express();
+const app = express()
 
 //middleware
+app.use(express.json())
 app.use((req, res, next) => {
-   console.log(req.path, req.method);
-   next();
-});
+  console.log(req.path, req.method)
+  next()
+})
 
 //routes
-app.get('/', (req, res) => {
-   res.json({ msg: 'Welcome to our app' });
-});
+app.use('/api/users', UsersRoutes)
+//app.use('/api/auth', AuthRoutes);
 
 //Connect to DB
 mongoose
-   .connect(process.env.MONG_URI)
-   .then(() => {
-      //listen for requests
-      app.listen(process.env.PORT, () => {
-         console.log('connected to db & listening on port', process.env.PORT);
-      });
-   })
-   .catch((err) => {
-      console.log(err);
-   });
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
