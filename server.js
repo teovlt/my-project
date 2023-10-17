94819502df0f173acdec4945c60c1311f11aeb9b
teovlt <teo.villet2@gmail.com>
2023-10-17 17:19:36 +0200

    add cors config

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ import 'dotenv/config'
 import mongoose from 'mongoose'
 import UsersRoutes from './src/routes/usersRoutes.js'
 import AuthRoutes from './src/routes/authRoutes.js'
+import cors from 'cors'
+
+//Autoriser les requetes dans le front
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: 'GET, POST, PUT, PATCH, DELETE',
+  preflightContinue: true,
+}
 
 //express app
 const app = express()
 
 //middleware
 app.use(express.json())
+app.use(cors(corsOptions))
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -28,7 +28,7 @@ const signUp = async (req, res) => {
     if (err.code === 11000) {
       return res.status(404).json({ error: 'Username already taken' })
     }
-    res.status(400).json({ error: err.message })
+    res.status(500).json({ error: err.message })
   }
 }
 
